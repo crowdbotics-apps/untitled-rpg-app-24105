d70ac8bcce6bbb45f0d20c13849cdb0730c8b4b7
Crowdbotics <team@crowdbotics.com>
2021-01-27 20:30:28 +0000

    Initial commit.

---
+++ ios/untitled_rpg_app_24105Tests/untitled_rpg_app_24105Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface untitled_rpg_app_24105 : XCTestCase
+
+@end
+
+@implementation untitled_rpg_app_24105
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
